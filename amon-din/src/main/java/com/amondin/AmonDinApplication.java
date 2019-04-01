75bb463bcd06f92a9f62fe509f964f40f5a1a4f6
Vladimir Sukmanov <33483821+tyzik89@users.noreply.github.com>
2019-04-01 21:21:46 +0300

    New Project

---
+++ amon-din/src/main/java/com/amondin/AmonDinApplication.java
@@ -0,0 +1,13 @@
+package com.amondin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmonDinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmonDinApplication.class, args);
+	}
+
+}
