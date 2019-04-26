5c7d24c6f8c992f95f979cbe4a5e479960b1d221
Vladimir Sukmanov <33483821+tyzik89@users.noreply.github.com>
2019-04-27 02:06:58 +0300

    Config + fix

--- amon-din/src/main/java/com/amondin/model/services/impl/BellServiceImpl.java
+++ amon-din/src/main/java/com/amondin/model/services/impl/BellServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class BellServiceImpl implements BellService {
 
     @Autowired
-    @Qualifier("jdbs_repository")
+    @Qualifier("jdbc_repository")
     private BellRepository bellRepository;
 
     @Override
