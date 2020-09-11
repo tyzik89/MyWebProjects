9348f6ebfabf3b08028fcabb7daeb1a8970f4ee5
Vladimir <work.vladimir.s@gmail.com>
2020-09-12 00:17:20 +0300

    Delete

--- amon-din/src/main/java/com/amondin/models/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amondin.models.repositories;
-
-import com.amondin.models.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findUserByUsername(String username);
-}
